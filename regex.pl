7b77a5c8ede3b8a1188f49684c6c456415dee230
u <u>
2025-04-09 18:37:50 +0300

    update

--- regex.pl
+++ regex.pl
@@ -3,6 +3,6 @@ s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\n~
 s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
 s~%gocheck~~g;
 s~%{_bindir}/wireguard-go~%{_bindir}/*~g;
-s~%gobuild -o %{gobuilddir}/bin/wireguard-go %{goipath}~~g;
+s~%gobuild -o %{gobuilddir}/bin/wireguard-go %{goipath}~mkdir examples~g;
 s~(%build.*)~\1\nrm version.go~g;
 s~Source:.*~%define stag %{?tag}%{?!tag:%commit}\nSource: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
